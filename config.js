85aabebc5166d17ebc110abff5e3c2ec64bb46ff
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-06-07 14:58:31 -0700

    Update add Facebook Oath support to enable authentication of my users and allowing them access to my server

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '2007566846138141',
+        clientSecret: '683f72d837628ef9357068afa998dcbb',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
